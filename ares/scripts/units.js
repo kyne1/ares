035832e60947553f9cb872020c596d119ed02e76
kyne1 <80266217+kyne1@users.noreply.github.com>
2021-03-09 10:13:49 -0500

    overriden drawWeapon() in UnitType.java
    broke it

--- ares/scripts/units.js
+++ ares/scripts/units.js
@@ -1,18 +1,44 @@
 //const a = Vars.content.getByName(ContentType.unit,"ares-ares");
 //print(Vars.content.getByName(ContentType.unit,"ares-ares").name);
-const a = extend(UnitType, "ares", {});
+const a = extend(UnitType, "ares", {
+  drawWeapons(unit){
+    unit.time;
+  }  
+});
+
 a.constructor = () => extend(UnitEntity, {});
 //Blocks.airFactory.plans = Blocks.airFactory.plans.put(UnitFactory.UnitPlan(a, 60 * 30, ItemStack.with(Items.copper, 2)));
 //secondary bullet frag
 
+const controllernew = extend(AIController, {
+
+});
+
+/*const turretnew = extend(Turret,{
+  updateShooting(){
+    if(reload >= reloadTime){
+        type = peekAmmo();
+
+        shoot(type);
+
+        reload = 0;
+    }else{
+        reload += delta() * peekAmmo().reloadMultiplier * baseReloadSpeed() *Math.random();
+    }
+  }
+});*/
+
+//event listernerns dont need loope
+
 const sbf = extend(BasicBulletType, {
   width: 5,
   height: 7,
-  lifetime: 43,
-  speed: 3,
-  splashDamageRadius: 10,
-  splashDamage: 3,
-  despawnEffect: Fx.none
+  lifetime: 30,
+  speed: 5,
+  splashDamageRadius: 20,
+  splashDamage: 5,
+  despawnEffect: Fx.none,
+  hitEffect: Fx.none
 });
 
 //sb = secondary bullet
@@ -23,17 +49,16 @@ const sb = extend(BasicBulletType, {
   speed: 12,
   drag: 0,
   damage: 70,
-  splashDamageRadius: 25,
-  splashDamage: 14,
   makeFire: true,
   status: StatusEffects.burning,
   lifetime: 28,
-  fragBullets: 7,
-  fragCone: 70,
+  fragBullets: 2,
+  fragCone: 30,
   //add frag
-  fragBullet: sbf
+  fragBullet: sbf,
+  despawnEffect: Fx.none,
+  hitEffect: Fx.none
 });
-
 
 //main gun shot -> minshot
 const slagfrag = extend(LiquidBulletType,{
@@ -54,17 +79,17 @@ const mainshot = extend(ArtilleryBulletType, {
   splashDamage: 350,
   status: StatusEffects.burning,
   lifetime: 190,
-  trailEffect: Fx.smoke,
+  trailEffect: Fx.artilleryTrail,
   hitEffect: Fx.massiveExplosion,
   keepVelocity: false,
   fragBullets: 13,
   fragBullet: slagfrag
 });
 
-
 //main gun
 const w1 = extendContent(Weapon, "main-cannon",{
   load(){this.super$load();this.region = Core.atlas.find("ares-main-cannon");},
+  
   rotate: true,
   rotateSpeed: 1.1,
   mirror: false,
@@ -85,9 +110,9 @@ a.weapons.add(w1);
 //secondaries
 for(let i = 0; i < 5; i++){
   for(let j = -1; j < 2; j += 2){
-let w2 = extendContent(Weapon, "secondaries", {
+var w2 = extendContent(Weapon, "secondaries", {
   //load sprite
-  load(){this.super$load();this.region = Core.atlas.find("ares-secondaries");},
+  load(){this.super$load();this.region = Core.atlas.find("ares-secondaries");}, 
   shootY: 10,
   reload: 9.5,
   x: 25*j,
@@ -103,10 +128,18 @@ let w2 = extendContent(Weapon, "secondaries", {
   mirror: false,
   bullet: sb
 });
+  /*if(w2.isShooting()){
+    this.reload = 1;
+  }*/
   a.weapons.add(w2);
 }}
+/*
+a.weapons.get(9).reload = 5;
 
-
+print("eeee");
+print(rld2);
+*/
+//a.onChange()
 
 
 /*for(let i = 0; i < Vars.content.units().size; i++){
